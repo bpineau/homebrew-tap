1a36944a092ccbf7b1056d27db9100b6ec5ffc9d
Benjamin Pineau <ben.pineau@gmail.com>
2018-05-14 14:58:12 +0200

    Update for katafygio 0.7.0

--- Formula/katafygio.rb
+++ Formula/katafygio.rb
@@ -1,9 +1,9 @@
 class Katafygio < Formula
   desc "Dump, or continuously backup Kubernetes objets as yaml files in git"
   homepage "https://github.com/bpineau/katafygio"
-  url "https://github.com/bpineau/katafygio/releases/download/v0.6.0/katafygio_0.6.0_darwin_amd64"
-  version "0.6.0"
-  sha256 "d135a83212852d7b560d49117af4fceace72756c34d060a4fbfb6a08994adc18"
+  url "https://github.com/bpineau/katafygio/releases/download/v0.7.0/katafygio_0.7.0_darwin_amd64"
+  version "0.7.0"
+  sha256 "ff515fcc9df9fdd18321d3580c2e1d447b933b1b893e1b31e33919fc71198791"
   bottle :unneeded
 
   def install
